474a03b9c0cf6a6ed0637f07fc96bf050434e442
loferga <laforgemateo@gmail.com>
2024-04-14 17:43:25 +0200

    gradle init

---
+++ src/main/java/fr/tmp/Main.java
@@ -0,0 +1,13 @@
+package fr.tmp;
+
+public class Main {
+   
+    public String getGreeting() {
+         return "Hello World, this is Main!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
