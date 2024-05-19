b480ec319c59a001145224de32474ebc47261922
loferga <laforgemateo@gmail.com>
2024-05-19 15:06:59 +0200

    temporary patch for scenarios

--- src/test/java/fr/call_of_rum/scenario/GUIScenario.java
+++ src/test/java/fr/call_of_rum/scenario/GUIScenario.java
@@ -8,11 +8,13 @@ import fr.call_of_rum.controller.BuyController;
 import fr.call_of_rum.controller.GameController;
 import fr.call_of_rum.controller.MoveController;
 import fr.call_of_rum.controller.TakeItemController;
+import fr.call_of_rum.controller.TriggerCellController;
 import fr.call_of_rum.controller.accessible.ActionController;
 import fr.call_of_rum.controller.accessible.ActionControllerImpl;
 import fr.call_of_rum.controller.accessible.BoardController;
 import fr.call_of_rum.controller.accessible.BoardControllerImpl;
 import fr.call_of_rum.controller.accessible.DiceControllerImpl;
+import fr.call_of_rum.controller.accessible.PlayerController;
 import fr.call_of_rum.model.board.Board;
 import fr.call_of_rum.model.board.BoardFactory;
 import fr.call_of_rum.model.item.ItemRegistry;
@@ -23,17 +25,19 @@ public abstract class GUIScenario {
 	
 	protected Dialog dialog;
 	protected FunctionalKernelAdapter boundary;
-	
+
 	protected DiceControllerImpl diceController;
 	protected GameController gameController;
 	protected TakeItemController takeItemController;
+	protected TriggerCellController triggerCellController;
 	protected MoveController moveController;
 	protected BuyController buyController;
 	protected ActionController actionController;
+	protected PlayerController playerController;
 	protected BoardController boardController;
 	
 	protected ItemRegistry itemRegistry;
-	protected BoardFactory boardFactory;
+	protected Board board;
 	protected Market market;
 	protected Pirate pirate1;
 	protected Pirate pirate2;
@@ -44,28 +48,30 @@ public abstract class GUIScenario {
     	
     	// model initialization
     	itemRegistry = new ItemRegistry();
-    	boardFactory = BoardFactory.getFactory();
     	market = new Market(List.of());
     	pirate1 = new Pirate("Jack Le Borgne", 0, 10);
     	pirate2 = new Pirate("Bill Jambe De Bois", 0, 10);
     	
     	// controller initialization
     	diceController = new DiceControllerImpl();
+    	takeItemController = new TakeItemController();
+    	triggerCellController = new TriggerCellController(boundary, takeItemController);
     	buyController = new BuyController(takeItemController, market);
-    	actionController = new ActionControllerImpl(gameController, buyController, takeItemController, moveController);
     	
     	// wiring IBoundary and IFunctionalKernel
     	boundary.setActionController(actionController);
 	}
 	
 	protected void start() {
-		Board board = boardFactory.build(itemRegistry);
+		if (board == null) {
+			board = BoardFactory.getDefaultBoard(itemRegistry);
+		}
     	board.addPirate(pirate1);
     	board.addPirate(pirate2);
-		
+
     	gameController = new GameController(boundary, diceController, board, pirate1, pirate2);
-    	takeItemController = new TakeItemController(board);
-    	moveController = new MoveController(boundary, diceController, takeItemController, board);
+    	moveController = new MoveController(diceController, triggerCellController, board);
+    	actionController = new ActionControllerImpl(gameController, buyController, moveController);
     	boardController = new BoardControllerImpl(gameController, board);
     	
     	boundary.setBoardController(boardController);
